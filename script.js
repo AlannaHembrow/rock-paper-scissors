4cd911a85583ae074bde9e902984b555850a863e
IvyWoof <al.hembrow@gmail.com>
2021-05-29 15:51:01 +0100

    Add round function to show scores and winner/loser

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let computerSelection;
+let playerSelection;
+
+function playerChoice() {
+    playerSelection =  "rock";
+    return playerSelection;
+}
+
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -13,25 +21,37 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == "rock" && computerSelection == "paper") || 
        (playerSelection == "paper" && computerSelection == "scissors" ) ||
        (playerSelection == "scissors" && computerSelection == "rock")) {
-        return result = "loser";
+        return "loss";
     } else if (playerSelection === computerSelection) {
-        return result = "draw";
+        return "tie";
     } else {
-        return result = "winner";
+        return "win";
     }
 }
-
 
 function game() {
-    if (result === "loser") {
-        console.log("you suck");
-    } else {
-        console.log("you best");
+    let lastResult;
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = playerChoice();
+        computerSelection = computerPlay();
+        lastResult = playRound(playerSelection, computerSelection);
+        playerScore += roundResult;
+        if (lastResult == "win") {
+            console.log("You win!") 
+            roundResult = 1;
+        } else if (lastResult == "loss") {
+            console.log("You lost!")
+            computerScore += 1;
+            roundResult = 0;
+        } else {
+            console.log("It was a tie!") 
+            roundResult = 0;
+        }
     }
+    console.log(`The results are in: ${playerScore} Your Score ${computerScore} The Computer`);
 }
 
-let result;
-const playerSelection = "paper";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
 game();
