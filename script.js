e8c537c5027ecd09a6f2049c76e66af178a73bda
IvyWoof <al.hembrow@gmail.com>
2021-05-29 13:44:41 +0100

    Add selection comparison and winner decleration

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function computerPlay() {
-    let winningChoice = Math.floor(Math.random() * 3);
-    if (winningChoice == 0) {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
         return "rock";
-    } else if (winningChoice == 1){
+    } else if (computerChoice == 1) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "paper") || 
+       (playerSelection == "paper" && computerSelection == "scissors" ) ||
+       (playerSelection == "scissors" && computerSelection == "rock")) {
+        return result = "loser";
+    } else if (playerSelection === computerSelection) {
+        return result = "draw";
+    } else {
+        return result = "winner";
+    }
+}
+
+
+function game() {
+    if (result === "loser") {
+        console.log("you suck");
+    } else {
+        console.log("you best");
+    }
+}
+
+let result;
+const playerSelection = "paper";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+game();
