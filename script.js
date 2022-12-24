400cb4e8f39db51d31bb075145602a78f8f05e4d
Alanna Hembrow <al.hembrow@gmail.com>
2022-12-24 14:54:46 +0000

    Added game function through buttons

--- script.js
+++ script.js
@@ -1,77 +1,80 @@
-let playerChoice = 0;
+let btns = document.querySelectorAll(".game__buttons-player");
+let score = 0;
+let computerScore = 0;
 
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
-    playerChoice = "rock";
-    newRound(playerChoice);
-});
-
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => {
-    playerChoice = "scissors";
-    newRound(playerChoice);
-});
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => {
-    playerChoice = "paper";
-    newRound(playerChoice);
-});
-
-function newRound(playerChoice) {
-    alert(playerChoice);
+for (i of btns) {
+    i.addEventListener('click', function() {
+        let playerSelection = this.innerHTML
+        document.querySelector('.msg').innerHTML = playerSelection;
+        game(playerSelection);
+    });
 }
 
-// function computerPlay() {
-//     let computerChoice = Math.floor(Math.random() * 3);
-//     if (computerChoice == 0) {
-//         return "rock";
-//     } else if (computerChoice == 1) {
-//         return "paper";
-//     } else {
-//         return "scissors";
-//     }
-// }
 
+function playRound(playerSelection) {
+    let computerChoice = computerRandom();
+        if ((playerSelection == "Rock" && computerChoice == "Paper") || 
+        (playerSelection == "Paper" && computerChoice == "Scissors" ) ||
+        (playerSelection == "Scissors" && computerChoice == "Rock")) {
+            let roundResult = "loss";
+            return roundResult;
+        } else if (playerSelection === computerChoice) {
+            let roundResult = "tie";
+            return roundResult;
+        } else {
+            let roundResult = "win";
+            return roundResult;
+        }
+}
 
-// function playRound(playerSelection, computerSelection) {
-//     let result = '';
-//         if ((playerSelection == "rock" && computerSelection == "paper") || 
-//         (playerSelection == "paper" && computerSelection == "scissors" ) ||
-//         (playerSelection == "scissors" && computerSelection == "rock")) {
-//             result = "loss";
-//         } else if (playerSelection === computerSelection) {
-//             result = "tie";
-//         } else {
-//             result = "win";
-//         }
-// }
+function computerRandom() {
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        return "Rock";
+    } else if (computerChoice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
 
-// function game() {
-//     let lastResult;
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         computerSelection = computerPlay();
-//         lastResult = playRound(playerSelection, computerSelection);
-//         if (lastResult == "win") {
-//             console.log("You win!") 
-//             playerScore += 1
-//         } else if (lastResult == "loss") {
-//             console.log("You lost!")
-//             computerScore += 1;
-//         } else {
-//             console.log("It was a tie!") 
-//         }
-//     }
-//     console.log(`The results are in: Your Score: ${playerScore} | The Computer: ${computerScore}`);
-//     if (playerScore > computerScore) {
-//         console.log(`You win the game!`);
-//     } else if (playerScore == computerScore) {
-//         console.log(`The game is a draw!`);
-//     } else {
-//         console.log(`You lose the game!`);
-//     }
-// }
+function resetGame() {
+    score = 0;
+    computerScore = 0;
+    console.log(`You have reset the game`)
+}
 
-// game();
+function game(playerSelection) {
+    roundResult = playRound(playerSelection);
+    for (let i = 0; i < 5; i++) {
+        if (roundResult == "win") {
+            score += 1;
+            console.log(`You win the round! The score is currently ${score} to ${computerScore}`);
+            break;
+        } else if (roundResult == "loss") {
+            computerScore += 1;
+            console.log(`You lost this round! The score is currently ${score} to ${computerScore}`);
+            break;
+        } else {
+            console.log(`It was a tie this round! The score is currently ${score} to ${computerScore}`);
+            break;
+        }
+    }
+
+    if (score == 5 || computerScore == 5) {
+        console.log(`The results are in: Your Score: ${score} | The Computer: ${computerScore}`);
+        if (score > computerScore) {
+            console.log(`You win the game!`);
+            resetGame(score, computerScore);
+        } else if (score == computerScore) {
+            console.log(`The game is a draw!`);
+            resetGame(score, computerScore);
+        } else {
+            console.log(`You lose the game!`);
+            resetGame(score, computerScore);
+        }
+    }
+}
+
+game();
+document.getElementById("reset").onclick = function() {resetGame()};
